529395addb302ba44a46ee3f795af954b2f6b968
Gobinath <slgobinath@gmail.com>
2017-12-01 15:15:15 -0500

    Attempt to fix the issue

--- safeeyes/BreakScreen.py
+++ safeeyes/BreakScreen.py
@@ -197,10 +197,10 @@ class BreakScreen(object):
                 # Fix flickering screen in KDE by setting opacity to 1
                 window.set_opacity(0.9)
 
+            window.move(x, y)
             window.stick()
             window.set_keep_above(True)
             window.present()
-            window.move(x, y)
             window.fullscreen()
 
     def __update_count_down(self, count):
